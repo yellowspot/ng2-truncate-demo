eeb8e4b8f913c2d8d71d2601c0ca7b62c73d25ba
Cristian Stügelmayer <cstugelmayer@yellowspot.com.ar>
2016-07-20 12:41:50 -0300

    Update mode trauncate pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { ng2TruncatePipe } from 'ng2-truncate';
+import { Ng2TruncatePipe } from 'ng2-truncate';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  pipes: [ng2TruncatePipe]
+  pipes: [Ng2TruncatePipe]
 })
 export class AppComponent {
   title = 'ng2-truncate-demo!';
