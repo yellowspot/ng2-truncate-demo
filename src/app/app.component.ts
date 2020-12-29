b15848388d41749fbf91538d33251485977c08ef
Cristian Stügelmayer <cristian@yellowspot.dev>
2020-12-29 18:41:47 -0300

    Add version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { version } from '../../package.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  version = version;
   title = 'ng2-truncate-demo!!';
 }
